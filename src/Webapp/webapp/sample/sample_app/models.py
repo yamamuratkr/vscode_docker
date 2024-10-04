860b15726fdf0af67a027b265ee1e96540e35564
t.y17gunner <t.y17gunner@gmail.com>
2024-10-04 17:32:02 +0900

    資材の修正

--- src/Webapp/webapp/sample/sample_app/models.py
+++ src/Webapp/webapp/sample/sample_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from viewflow.fields import CompositeKey
 
 # Create your models here.
 class SampleText(models.Model):
+    id = CompositeKey(columns=['sample_text'])
     sample_text = models.TextField()
 
     class Meta:
