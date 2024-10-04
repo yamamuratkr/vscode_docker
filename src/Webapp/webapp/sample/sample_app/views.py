61d2b2376c7a40357fe968ce072c89d7232f2e33
t.y17gunner <t.y17gunner@gmail.com>
2024-10-04 14:26:28 +0900

    index画面の作成

--- src/Webapp/webapp/sample/sample_app/views.py
+++ src/Webapp/webapp/sample/sample_app/views.py
@@ -5,4 +5,9 @@ from sample_app.models import SampleText
 # Create your views here.
 def index(request):
     index_text = SampleText.objects.get().sample_text
-    return HttpResponse(index_text)
+
+    params = {
+        "index_text": index_text
+    }
+
+    return render(request, "index.html", params)
